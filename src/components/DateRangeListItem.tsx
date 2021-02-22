49d09d18cfd0bc40e0e0563bfdfbc2ed6be2b6d5
Ahmad Safar <ahmadsafar88@gmail.com>
2021-02-22 12:58:18 +0800

    Update dateRange styles

--- src/components/DateRangeListItem.tsx
+++ src/components/DateRangeListItem.tsx
@@ -1,4 +1,6 @@
 
+import styled from 'styled-components'
+import tw from 'twin.macro'
 import { Button } from '../styles'
 
 type Props = {
@@ -6,13 +8,20 @@ type Props = {
   index: number,
 }
 
+const Div1 = styled.div`
+  ${tw`font-normal`}
+`
+const Div2 = styled.div`
+  ${tw`font-bold`}
+`
+
 export const DateRangeListItem = ({ dateRange, index }: Props) => {
   return (
     <Button roundedFull dtRange
       hover={index === 0}
       disabled={dateRange.ddd === 'Sat' || dateRange.ddd === 'Sun'}>
-      <div>{dateRange.ddd}</div>
-      <div>{dateRange.DD}</div>
+      <Div1>{dateRange.ddd}</Div1>
+      <Div2>{dateRange.DD}</Div2>
     </Button >
   )
 }
